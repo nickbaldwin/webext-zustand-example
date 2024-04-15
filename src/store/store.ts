e4dc2df001cbe5b21a2d2a6cd564e732197261c3
nickbaldwin <nick.with.a.z@gmail.com>
2024-04-15 13:47:39 +1000

    refactor state

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,22 +2,27 @@ import { create } from 'zustand';
 import { includeChromeStore } from 'zustand-chrome-local-storage';
 
 import { wrapStore } from 'webext-zustand';
+import { version } from './schema';
 
 // wrapStore: <T>(store: StoreApi<T>) => Promise<void>;
 
-export interface BearState {
+export interface State {
+    version: number;
     bears: number;
     increase: (by: number) => void;
+    lastMark: string;
 }
 
-export const useBearStore = create<BearState>()(
+export const useStore = create<State>()(
     // @ts-expect-error type?
     includeChromeStore((set) => ({
+        version,
         bears: 0,
         increase: (by) => set((state) => ({ bears: state.bears + by })),
+        lastMark: '',
     }))
 );
 
-export const storeReadyPromise = wrapStore(useBearStore);
+export const storeReadyPromise = wrapStore(useStore);
 
-export default useBearStore;
+export default useStore;

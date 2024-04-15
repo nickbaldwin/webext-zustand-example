8bbac39b3d727b9e4d9c678deaca0d46e5622a88
nickbaldwin <nick.with.a.z@gmail.com>
2024-04-15 11:47:55 +1000

    get tab info

--- src/popup/PC.tsx
+++ src/popup/PC.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import { BearState, useBearStore } from '../store/store';
+import { parseUrl } from '../helpers/domain';
 
 export const PC: () => React.JSX.Element = () => {
     const bears = useBearStore((state: BearState) => state.bears);
     const increase = useBearStore((state: BearState) => state.increase);
+
+    const [tabInfo, setTabInfo] = React.useState('');
+
+    const addTab = async (): Promise<void> => {
+        const [tab] = await chrome.tabs.query({
+            active: true,
+            currentWindow: true,
+        });
+        const { url = '', title = '', favIconUrl = '' } = tab;
+
+        // todo - deal with url errors
+        const domain = parseUrl(url) || '';
+        console.log(url, title, favIconUrl, domain);
+
+        setTabInfo(title);
+    };
 
     return (
         <div>
@@ -12,6 +29,12 @@ export const PC: () => React.JSX.Element = () => {
                 <span>Bears: {bears}</span>
                 <br />
                 <button onClick={() => increase(1)}>Increment +</button>
+
+                <br />
+
+                <br />
+                <button onClick={addTab}>get tab info +</button>
+                <span>tabinfo?: {tabInfo}</span>
             </div>
         </div>
     );

77755dbab3cc2e22a64c1da6c8ac89e6ea842f5c
nickbaldwin <nick.with.a.z@gmail.com>
2024-04-15 11:54:49 +1000

    installed and using zustand-chrome-local-storage

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,8 @@
 import { create } from 'zustand';
+import { includeChromeStore } from 'zustand-chrome-local-storage';
+
 import { wrapStore } from 'webext-zustand';
+
 // wrapStore: <T>(store: StoreApi<T>) => Promise<void>;
 
 export interface BearState {
@@ -7,10 +10,13 @@ export interface BearState {
     increase: (by: number) => void;
 }
 
-export const useBearStore = create<BearState>()((set) => ({
-    bears: 0,
-    increase: (by) => set((state) => ({ bears: state.bears + by })),
-}));
+export const useBearStore = create<BearState>()(
+    // @ts-expect-error type?
+    includeChromeStore((set) => ({
+        bears: 0,
+        increase: (by) => set((state) => ({ bears: state.bears + by })),
+    }))
+);
 
 export const storeReadyPromise = wrapStore(useBearStore);
 
